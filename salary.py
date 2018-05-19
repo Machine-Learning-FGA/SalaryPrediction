bcc9bccb9a4ec682eaaa2936b8f4651810196656
Victor Navarro <victor.matias.navarro@gmail.com>
2018-05-18 22:20:35 -0300

    Creating csv to run machine learn alg on
    Co-authored-by: Victor Navarro <victor.matias.navarro@gmail.com>
    Co-authored-by: Felipe Hargreaves <fhargreaves00@gmail.com>
    Co-authored-by: Guilherme de Lyra <guilyra12@gmail.com>
    Co-authored-by: Clarissa Borges <clarissalimab08@gmail.com>
    Co-authored-by: Bruna Pinos <bruna.pinos@gmail.com>
    Co-authored-by: Fabio Teixeira <fabio1079@gmail.com>
    Co-authored-by: Eduardo Vital <vitaldu@gmail.com>

--- salary.py
+++ salary.py
@@ -16,51 +16,36 @@ import zipfile
 import scipy.sparse as sp
 import csv
 
-
 columns_to_vectorize = {
     'Title': 6,
-    # 'FullDescription': 200,
-    # 'LocationRaw': 10,
-    # 'LocationNormalized':10
+    'FullDescription': 200,
+    'LocationRaw': 10,
+    'LocationNormalized':10
 }
 
+NUMBER_OF_ROWS = 100000
+
 new_file = open("Vectorized.csv","w")
-array = []
+writer = csv.writer(new_file, delimiter=',')
 
 with zipfile.ZipFile('Train_rev1.zip') as myzip:
     with myzip.open('Train_rev1.csv') as myfile:
-        df = pd.read_csv(myfile, nrows=1000)
+        df = pd.read_csv(myfile, nrows=NUMBER_OF_ROWS)
 
-writer = csv.writer(new_file, delimiter=',')
+definite_row = []
 
+for i in range(0, NUMBER_OF_ROWS):
+    definite_row.append([])
 
 for key in columns_to_vectorize:
-    count_vectorizer = CountVectorizer(max_features=columns_to_vectorize['Title'], stop_words='english')
-    transformed = count_vectorizer.fit_transform(df['Title'].head(1000)).toarray()
-    for value in transformed:
-        print(count_vectorizer.vocabulary_)
-        print(value)
-        writer.writerow(value)
-        # new_file.write(str(value) + ',')
-    # array.append(transformed)
-    # print(array)
-    # new_file.write(transformed)
+    count_vectorizer = CountVectorizer(max_features=columns_to_vectorize[key], stop_words='english')
+    transformed = count_vectorizer.fit_transform(df[key].values.astype('U')).toarray()
+    for index, value in enumerate(transformed):
+        value = value.tolist()
+        definite_row[index].extend(value)
+
+for i in range(0, NUMBER_OF_ROWS):
+    definite_row[i].extend([str(df['SalaryNormalized'][i])])
+    writer.writerow(definite_row[i])
 
 new_file.close()
-
-# df['ContractType'] = df['ContractType'].replace(['part_time'], 0)
-# df['ContractType'] = df['ContractType'].replace(['full_time'], 1)
-# df['ContractTime'] = df['ContractTime'].replace(['permanent'], 0)
-# df['ContractTime'] = df['ContractTime'].replace(['contract'], 1)
-#
-#
-# X = df.drop('SalaryNormalized', axis=1)
-# y = df['SalaryNormalized']
-#
-# print(X)
-#
-# X = X[:, 10]
-# X = X.reshape(-1, 1)
-# y = y.reshape(-1, 1)
-#
-# print(X)
