2376f438ec57cb0ed7b2102b612b70ab257d4ff6
Victor Navarro <victor.matias.navarro@gmail.com>
2018-05-20 21:58:23 -0300

    Added more columns to data treatment
    Co-authored-by: Victor Navarro <victor.matias.navarro@gmail.com>
    Co-authored-by: Felipe Hargreaves <fhargreaves00@gmail.com>
    Co-authored-by: João Vitor <joaovytor0@gmail.com>

--- data_treatment.py
+++ data_treatment.py
@@ -16,14 +16,25 @@ import zipfile
 import scipy.sparse as sp
 import csv
 
+def number_of_columns():
+    total = 0
+    for key in columns_to_vectorize:
+        total += columns_to_vectorize[key]
+    return total
+
 columns_to_vectorize = {
-    'Title': 6,
-    'FullDescription': 200,
-    'LocationRaw': 10,
-    'LocationNormalized':10
+    'Title': 500,
+    'FullDescription': 3000,
+    'LocationRaw': 100,
+    'LocationNormalized': 100,
+    'ContractType': 2,
+    'ContractTime': 2,
+    'Company': 500,
+    'Category': 20,
+    'SourceName': 100
 }
 
-NUMBER_OF_ROWS = 100
+NUMBER_OF_ROWS = 50000
 
 new_file = open("Vectorized.csv","w")
 writer = csv.writer(new_file, delimiter=',')
@@ -33,14 +44,12 @@ with zipfile.ZipFile('Train_rev1.zip') as myzip:
         df = pd.read_csv(myfile, nrows=NUMBER_OF_ROWS)
 
 definite_row = []
+definite_row.append([str(x) for x in range(0, number_of_columns())])
+definite_row[0].append('Salary')
 
-for i in range(0, NUMBER_OF_ROWS + 1):
+for i in range(0, NUMBER_OF_ROWS):
     definite_row.append([])
 
-for i in range(0, 226):
-    definite_row[0].append(i)
-
-definite_row[0].append('Salary')
 writer.writerow(definite_row[0])
 
 for key in columns_to_vectorize:
@@ -48,10 +57,10 @@ for key in columns_to_vectorize:
     transformed = count_vectorizer.fit_transform(df[key].values.astype('U')).toarray()
     for index, value in enumerate(transformed):
         value = value.tolist()
-        definite_row[index].extend(value)
+        definite_row[index+1].extend(value)
 
-for i in range(0, NUMBER_OF_ROWS-1):
-    definite_row[i + 1].extend([str(df['SalaryNormalized'][i])])
-    writer.writerow(definite_row[i + 1])
+for i in range(0, NUMBER_OF_ROWS):
+    definite_row[i+1].extend([str(df['SalaryNormalized'][i])])
+    writer.writerow(definite_row[i+1])
 
 new_file.close()
--- predict.py
+++ predict.py
@@ -4,18 +4,15 @@ import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score, median_absolute_error
-
+from sklearn.metrics import explained_variance_score
 
 df = pd.read_csv('Vectorized.csv')
 
-
+df['Salary'] = np.log(df['Salary'])
 X = df.drop('Salary', axis=1).values
 y = df['Salary'].values
 
-print(X)
-print(y)
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 reg = linear_model.LinearRegression()
 reg.fit(X_train, y_train)
@@ -25,3 +22,4 @@ result = reg.predict(X_test)
 print('Coefficients: \n', reg.coef_)
 print("median_absolute_error: %.2f" % median_absolute_error(y_test, result))
 print('Variance score: %.2f' % r2_score(y_test, result))
+print(explained_variance_score(y_test, result))
